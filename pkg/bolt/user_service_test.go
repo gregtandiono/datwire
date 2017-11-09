d9483872ac5c247f52cee2ffb8985d3c76ba17a4
Gregory Tandiono <gregtandiono@gmail.com>
2017-11-09 16:59:01 +0700

    WIP setup and teardown

--- pkg/bolt/user_service_test.go
+++ pkg/bolt/user_service_test.go
@@ -1,1 +1,19 @@
 package bolt_test
+
+import (
+	"datwire/pkg/bolt"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type UserServiceTestSuite struct {
+	suite.Suite
+	userService *bolt.UserService
+}
+
+func (suite *UserServiceTestSuite) SetupSuite() {
+	suite.userService = bolt.NewUserService()
+}
+
+func (suite *UserServiceTestSuite) TearDownSuite() {
+}
