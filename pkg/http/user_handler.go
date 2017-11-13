ce1cb6dd109ca6d16e9335f19a6e7da7deaeebc2
Gregory Tandiono <gregtandiono@gmail.com>
2017-11-13 11:49:33 +0700

    added create user handler in user_handler

--- pkg/http/user_handler.go
+++ pkg/http/user_handler.go
@@ -1,8 +1,10 @@
 package http
 
 import (
+	"datwire/pkg/apps/user"
 	"datwire/pkg/bolt"
 	"datwire/pkg/shared"
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
@@ -45,6 +47,28 @@ func (h *UserHandler) handleGetUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *UserHandler) handleCreateUser(w http.ResponseWriter, r *http.Request) {
+	var user *user.User
+	// if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+	// 	shared.EncodeError(w, err, 400, h.Logger)
+	// 	return
+	// } else if err := h.UserService.CreateUser(user); err != nil {
+	// 	shared.EncodeError(w, err, 400, h.Logger)
+	// } else {
+	// 	shared.EncodeJSON(w, &shared.ResponseTemplate{Message: "success"}, h.Logger)
+	// }
+
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		shared.EncodeError(w, err, 400, h.Logger)
+		return
+	}
+
+	err = h.UserService.CreateUser(user)
+	if err != nil {
+		shared.EncodeError(w, err, 400, h.Logger)
+	} else {
+		shared.EncodeJSON(w, &shared.ResponseTemplate{Message: "success"}, h.Logger)
+	}
 }
 
 func (h *UserHandler) handleSetName(w http.ResponseWriter, r *http.Request) {
