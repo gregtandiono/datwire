1493175fa1ab4ec7badbaeecf4ff316db75ee9fa
Gregory Tandiono <gregtandiono@gmail.com>
2017-11-14 17:09:03 +0700

    added set name gateway handler to user service

--- pkg/http/gateway.go
+++ pkg/http/gateway.go
@@ -1,6 +1,10 @@
 package http
 
 import (
+	"datwire/pkg/shared"
+	"encoding/json"
+	"errors"
+	"log"
 	"net/http"
 	"strings"
 )
@@ -21,5 +25,51 @@ func (g *GatewayHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch true {
 	case strings.HasPrefix(r.URL.Path, "/users"):
 		g.UserGateway.ServeHTTP(w, r)
+	}
+}
+
+func gatewayHandlerFactory(reqMethod, targetBaseURL, endpoint string, w http.ResponseWriter, r *http.Request, l *log.Logger) {
+	var responseBody *shared.ResponseTemplate
+	switch reqMethod {
+	case "PUT":
+		client := &http.Client{}
+		request, err := http.NewRequest("PUT", targetBaseURL+endpoint, r.Body)
+		if err != nil {
+			shared.EncodeError(w, err, 500, l)
+		}
+		request.Header.Set("Content-Type", "application/json")
+		response, err := client.Do(request)
+		if err != nil {
+			shared.EncodeError(w, err, 500, l)
+		}
+
+		err = json.NewDecoder(response.Body).Decode(&responseBody)
+		if responseBody.Message == "fail" {
+			shared.EncodeError(w, errors.New(responseBody.Error), 400, l)
+		} else {
+			shared.EncodeJSON(w, responseBody, l)
+		}
+	case "POST":
+		response, err := http.Post(targetBaseURL+endpoint, "application/json", r.Body)
+		if err != nil {
+			shared.EncodeError(w, err, 500, l)
+		}
+		err = json.NewDecoder(response.Body).Decode(&responseBody)
+		if responseBody.Message == "fail" {
+			shared.EncodeError(w, errors.New(responseBody.Error), 400, l)
+		} else {
+			shared.EncodeJSON(w, responseBody, l)
+		}
+	case "GET":
+		response, err := http.Get(targetBaseURL + endpoint)
+		if err != nil {
+			shared.EncodeError(w, err, 500, l)
+		}
+		err = json.NewDecoder(response.Body).Decode(&responseBody)
+		if responseBody.Message == "fail" {
+			shared.EncodeError(w, errors.New(responseBody.Error), 400, l)
+		} else {
+			shared.EncodeJSON(w, responseBody, l)
+		}
 	}
 }
--- pkg/http/user_gateway.go
+++ pkg/http/user_gateway.go
@@ -1,9 +1,6 @@
 package http
 
 import (
-	"datwire/pkg/shared"
-	"encoding/json"
-	"errors"
 	"log"
 	"net/http"
 	"os"
@@ -26,6 +23,8 @@ func NewUserGateway() *UserGateway {
 	}
 
 	g.Handle("/users/{id}", http.HandlerFunc(g.handleGetUser)).Methods("GET")
+	g.Handle("/users/{id}", http.HandlerFunc(g.handleSetName)).Methods("PUT")
+	g.Handle("/users", http.HandlerFunc(g.handleGetUsers)).Methods("GET")
 	g.Handle("/users", http.HandlerFunc(g.handleCreateUser)).Methods("POST")
 
 	return g
@@ -44,27 +43,25 @@ func (g *UserGateway) handleGetUser(w http.ResponseWriter, r *http.Request) {
 
 }
 
-// func (g *UserGateway) handleGetUsers(w http.ResponseWriter, r *http.Request) {
-// 	vals := r.FormValue("username")
-// 	if vals != "" {
-// 		if userID, err := h.UserService.CheckIfUserExists(vals); err != nil {
-// 			shared.EncodeError(w, err, 400, h.Logger)
-// 		} else {
-// 			shared.EncodeJSON(w, &shared.ResponseTemplate{Message: "success", Data: userID}, h.Logger)
-// 		}
+func (g *UserGateway) handleGetUsers(w http.ResponseWriter, r *http.Request) {
+	vals := r.FormValue("username")
 
-// 	} else {
-// 		if users, err := h.UserService.Users(); err != nil {
-// 			shared.EncodeError(w, err, 400, h.Logger)
-// 		} else {
-// 			shared.EncodeJSON(
-// 				w,
-// 				&shared.ResponseTemplate{Message: "success", Data: users},
-// 				h.Logger,
-// 			)
-// 		}
-// 	}
-// }
+	if vals != "" {
+		gatewayHandlerFactory(
+			"GET",
+			"http://localhost:1337",
+			"/users?username="+vals,
+			w, r, g.Logger,
+		)
+	} else {
+		gatewayHandlerFactory(
+			"GET",
+			"http://localhost:1337",
+			"/users",
+			w, r, g.Logger,
+		)
+	}
+}
 
 func (g *UserGateway) handleCreateUser(w http.ResponseWriter, r *http.Request) {
 	gatewayHandlerFactory(
@@ -75,56 +72,17 @@ func (g *UserGateway) handleCreateUser(w http.ResponseWriter, r *http.Request) {
 	)
 }
 
-func gatewayHandlerFactory(reqMethod, targetBaseURL, endpoint string, w http.ResponseWriter, r *http.Request, l *log.Logger) {
-	var responseBody *shared.ResponseTemplate
-	switch reqMethod {
-	case "POST":
-		response, err := http.Post(targetBaseURL+endpoint, "application/json", r.Body)
-		if err != nil {
-			shared.EncodeError(w, err, 500, l)
-		}
-		err = json.NewDecoder(response.Body).Decode(&responseBody)
-		if responseBody.Message == "fail" {
-			shared.EncodeError(w, errors.New(responseBody.Error), 400, l)
-		} else {
-			shared.EncodeJSON(w, responseBody, l)
-		}
-	case "GET":
-		response, err := http.Get(targetBaseURL + endpoint)
-		if err != nil {
-			shared.EncodeError(w, err, 500, l)
-		}
-		err = json.NewDecoder(response.Body).Decode(&responseBody)
-		if responseBody.Message == "fail" {
-			shared.EncodeError(w, errors.New(responseBody.Error), 400, l)
-		} else {
-			shared.EncodeJSON(w, responseBody, l)
-		}
-	}
+func (g *UserGateway) handleSetName(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	userID := vars["id"]
+
+	gatewayHandlerFactory(
+		"PUT",
+		"http://localhost:1337",
+		"/users/"+userID,
+		w, r, g.Logger,
+	)
 }
-
-// type nameSetterReq struct {
-// 	Name string `json:"name"`
-// }
-
-// func (g *UserGateway) handleSetName(w http.ResponseWriter, r *http.Request) {
-// 	vars := mux.Vars(r)
-// 	userID := uuid.FromStringOrNil(vars["id"])
-
-// 	var reqBody *nameSetterReq
-// 	err := json.NewDecoder(r.Body).Decode(&reqBody)
-// 	if err != nil {
-// 		shared.EncodeError(w, err, 400, h.Logger)
-// 		return
-// 	}
-
-// 	err = h.UserService.SetName(userID, reqBody.Name)
-// 	if err != nil {
-// 		shared.EncodeError(w, err, 400, h.Logger)
-// 	} else {
-// 		shared.EncodeJSON(w, &shared.ResponseTemplate{Message: "success"}, h.Logger)
-// 	}
-// }
 
 // func (g *UserGateway) handleDeleteUser(w http.ResponseWriter, r *http.Request) {
 // 	vars := mux.Vars(r)
