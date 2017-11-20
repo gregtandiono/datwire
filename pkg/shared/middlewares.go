3bfdbad6169f070162ec54560c5fdfd0d7708c78
Gregory Tandiono <gregtandiono@gmail.com>
2017-11-20 12:10:11 +0700

    added token parser and jwt middleware

--- pkg/shared/middlewares.go
+++ pkg/shared/middlewares.go
@@ -1,23 +1,34 @@
 package shared
 
-// func tokenParser(authHeader string) string {
-// 	split := strings.Split(authHeader, " ")
-// 	token := split[1]
-// 	return token
-// }
+import (
+	"encoding/json"
+	"net/http"
+	"strings"
 
-// func myJWTMiddleware() *jwtmiddleware.JWTMiddleware {
-// 	env := anakkandang.GetEnvironmentVariables()
-// 	jwtMiddleware := jwtmiddleware.New(jwtmiddleware.Options{
-// 		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
-// 			return []byte(env.Hash), nil
-// 		},
-// 		ErrorHandler: func(w http.ResponseWriter, r *http.Request, err string) {
-// 			w.WriteHeader(401)
-// 			json.NewEncoder(w).Encode(&ResponseTemplate{Message: "fail", Error: "required authorization token is invalid"})
-// 		},
-// 		SigningMethod: jwt.SigningMethodHS256,
-// 	})
+	jwtmiddleware "github.com/auth0/go-jwt-middleware"
+	jwt "github.com/dgrijalva/jwt-go"
+)
 
-// 	return jwtMiddleware
-// }
+// TokenParser parses token from request header
+func TokenParser(authHeader string) string {
+	split := strings.Split(authHeader, " ")
+	token := split[1]
+	return token
+}
+
+// JWTMiddleware is a pluggable middleware to an existing route
+// to check whether there is a valid token in a request header.
+func JWTMiddleware(secretHash string) *jwtmiddleware.JWTMiddleware {
+	jwtMiddleware := jwtmiddleware.New(jwtmiddleware.Options{
+		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+			return []byte(secretHash), nil
+		},
+		ErrorHandler: func(w http.ResponseWriter, r *http.Request, err string) {
+			w.WriteHeader(401)
+			json.NewEncoder(w).Encode(&ResponseTemplate{Message: "fail", Error: "required authorization token is invalid"})
+		},
+		SigningMethod: jwt.SigningMethodHS256,
+	})
+
+	return jwtMiddleware
+}
